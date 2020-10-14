20bc89c66be97d77ebef434600c94bd704c93530
Boy Sandy G. A <dev@renodesper.com>
2020-10-15 01:07:43 +0700

    Add context utility to set and get requestID

---
+++ util/ctx/ctx.go
@@ -0,0 +1,36 @@
+package ctx
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+// CtxKey ...
+type CtxKey int
+
+const (
+	// CtxRequestID ...
+	CtxRequestID CtxKey = iota
+)
+
+// GetRequestID ...
+func GetRequestID(ctx context.Context) string {
+	res, _ := ctx.Value(CtxRequestID).(string)
+	return res
+}
+
+// SetRequestID ...
+func SetRequestID(ctx context.Context, val string) context.Context {
+	if val == "" {
+		val = uuid.New().String()
+	}
+	return context.WithValue(ctx, CtxRequestID, val)
+}
+
+// FromHTTPRequest ...
+func FromHTTPRequest(ctx context.Context, r *http.Request) context.Context {
+	ctx = SetRequestID(ctx, r.Header.Get("X-Request-Id"))
+	return ctx
+}
---
+++ util/ctx/ctx_test.go
@@ -0,0 +1,48 @@
+package ctx
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRequestID(t *testing.T) {
+	requestID := uuid.New().String()
+	ctx := context.Background()
+
+	ctx = SetRequestID(ctx, requestID)
+	c := GetRequestID(ctx)
+	assert.Equal(t, requestID, c)
+}
+
+func TestSetRequestID(t *testing.T) {
+	ctx := context.Background()
+
+	// NOTE: Defined requestID
+	requestID := "qwertyuiop"
+	ctx = SetRequestID(ctx, requestID)
+	s := GetRequestID(ctx)
+	assert.Equal(t, requestID, s)
+
+	// NOTE: Empty string as requestID
+	ctx = SetRequestID(ctx, "")
+	s = GetRequestID(ctx)
+	assert.NotEmpty(t, s)
+}
+
+func TestFromHTTPRequest(t *testing.T) {
+	ctx := context.Background()
+	requestID := "qwertyuiop"
+	header := map[string][]string{
+		"X-Request-Id": {requestID},
+	}
+
+	ctx = FromHTTPRequest(ctx, &http.Request{
+		Header: header,
+	})
+	s := GetRequestID(ctx)
+	assert.Equal(t, requestID, s)
+}
