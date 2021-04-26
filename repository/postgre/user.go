ba10af4ca0576094bcfef677f6f6e0985eb0edac
Boy Sandy Gladies Arriezona <dev@renodesper.com>
2021-04-26 14:46:15 +0700

    Feat: Add several functions into User repository
    - GetUserByID
    - GetUserByEmail
    - GetUserByUsername
    - CreateUser
    - UpdateUser
    - SetAccessToken
    - SetUserStatus
    - SetUserRole
    - SetUserExpiry
    - DeleteUser

--- repository/postgre/user.go
+++ repository/postgre/user.go
@@ -3,6 +3,7 @@ package postgre
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/go-pg/pg/v10"
 	"github.com/google/uuid"
@@ -15,8 +16,16 @@ type (
 	UserRepository interface {
 		GetAllUsers(ctx context.Context, sortBy string, sort string, skip int, limit int) ([]repository.User, error)
 		GetAllUsersByCursor(ctx context.Context, cursor string, direction string, limit int, sort string) ([]repository.User, error)
-		GetUser(ctx context.Context, userID uuid.UUID) (*repository.User, error)
-		CreateUser(ctx context.Context, user *repository.User) (uuid.UUID, error)
+		GetUserByID(ctx context.Context, userID uuid.UUID) (*repository.User, error)
+		GetUserByEmail(ctx context.Context, email string) (*repository.User, error)
+		GetUserByUsername(ctx context.Context, username string) (*repository.User, error)
+		CreateUser(ctx context.Context, userPayload *repository.User) (*repository.User, error)
+		UpdateUser(ctx context.Context, userID uuid.UUID, userPayload map[string]interface{}) (*repository.User, error)
+		SetAccessToken(ctx context.Context, userID uuid.UUID, accessToken string, refreshToken string) (*repository.User, error)
+		SetUserStatus(ctx context.Context, userID uuid.UUID, isActive bool) (*repository.User, error)
+		SetUserRole(ctx context.Context, userID uuid.UUID, isAdmin bool) (*repository.User, error)
+		SetUserExpiry(ctx context.Context, userID uuid.UUID, expiredAt time.Time) (*repository.User, error)
+		DeleteUser(ctx context.Context, userID uuid.UUID) (*repository.User, error)
 	}
 
 	UserRepo struct {
@@ -45,7 +54,7 @@ func (ur *UserRepo) GetAllUsers(ctx context.Context, sortBy string, sort string,
 
 	err := ur.Db.Model(&users).Limit(limit).Offset(skip).Order(order).Select()
 	if err != nil {
-		return nil, err
+		return nil, errors.FailedUsersFetch.AppendError(err)
 	}
 
 	return users, nil
@@ -61,23 +70,23 @@ func (ur *UserRepo) GetAllUsersByCursor(ctx context.Context, sort string, direct
 		return nil, err
 	}
 
-	q := ur.Db.Model(&users)
+	sql := ur.Db.Model(&users)
 
 	if sort == "DESC" {
 		if direction == "next" {
-			q.Where("created_at <= ?", createdAt).Where("id < ?", userID).Order("created_at DESC")
+			sql.Where("created_at <= ?", createdAt).Where("id < ?", userID).Order("created_at DESC")
 		} else {
-			q.Where("created_at >= ?", createdAt).Where("id > ?", userID).Order("created_at ASC")
+			sql.Where("created_at >= ?", createdAt).Where("id > ?", userID).Order("created_at ASC")
 		}
 	} else {
 		if direction == "next" {
-			q.Where("created_at >= ?", createdAt).Where("id > ?", userID).Order("created_at ASC")
+			sql.Where("created_at >= ?", createdAt).Where("id > ?", userID).Order("created_at ASC")
 		} else {
-			q.Where("created_at <= ?", createdAt).Where("id < ?", userID).Order("created_at DESC")
+			sql.Where("created_at <= ?", createdAt).Where("id < ?", userID).Order("created_at DESC")
 		}
 	}
 
-	err = q.Limit(limit).Select()
+	err = sql.Limit(limit).Select()
 	if err != nil {
 		return nil, err
 	}
@@ -93,24 +102,131 @@ func (ur *UserRepo) GetAllUsersByCursor(ctx context.Context, sort string, direct
 	return users, nil
 }
 
-// GetUser ...
-func (ur *UserRepo) GetUser(ctx context.Context, userID uuid.UUID) (*repository.User, error) {
+// GetUserByID ...
+func (ur *UserRepo) GetUserByID(ctx context.Context, userID uuid.UUID) (*repository.User, error) {
 	user := repository.User{}
+
 	err := ur.Db.Model(&user).Where("id = ?", userID).Select()
 	if err != nil {
-		return nil, err
+		return nil, errors.FailedUserFetch.AppendError(err)
 	}
+
+	return &user, nil
+}
+
+// GetUserByEmail ...
+func (ur *UserRepo) GetUserByEmail(ctx context.Context, email string) (*repository.User, error) {
+	user := repository.User{}
+
+	err := ur.Db.Model(&user).Where("email = ?", email).Select()
+	if err != nil {
+		return nil, errors.FailedUserFetch.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+// GetUserByUsername ...
+func (ur *UserRepo) GetUserByUsername(ctx context.Context, username string) (*repository.User, error) {
+	user := repository.User{}
+
+	err := ur.Db.Model(&user).Where("username = ?", username).Select()
+	if err != nil {
+		return nil, errors.FailedUserFetch.AppendError(err)
+	}
+
 	return &user, nil
 }
 
 // CreateUser ...
-func (ur *UserRepo) CreateUser(ctx context.Context, user *repository.User) (uuid.UUID, error) {
-	var userID uuid.UUID
+func (ur *UserRepo) CreateUser(ctx context.Context, userPayload *repository.User) (*repository.User, error) {
+	var user repository.User
 
-	_, err := ur.Db.WithContext(ctx).Model(user).Returning("id").Insert(&userID)
+	_, err := ur.Db.WithContext(ctx).Model(userPayload).Returning("*").Insert(&user)
 	if err != nil {
-		return uuid.UUID{}, errors.FailedUserCreate.AppendError(err)
+		return nil, errors.FailedUserCreate.AppendError(err)
 	}
 
-	return userID, nil
+	return &user, nil
+}
+
+func (ur *UserRepo) UpdateUser(ctx context.Context, userID uuid.UUID, userPayload map[string]interface{}) (*repository.User, error) {
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserUpdate.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) SetAccessToken(ctx context.Context, userID uuid.UUID, accessToken string, refreshToken string) (*repository.User, error) {
+	userPayload := map[string]interface{}{
+		"access_token":  accessToken,
+		"refresh_token": refreshToken,
+	}
+
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserUpdate.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) SetUserStatus(ctx context.Context, userID uuid.UUID, isActive bool) (*repository.User, error) {
+	userPayload := map[string]interface{}{
+		"is_active": isActive,
+	}
+
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserUpdate.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) SetUserRole(ctx context.Context, userID uuid.UUID, isAdmin bool) (*repository.User, error) {
+	userPayload := map[string]interface{}{
+		"is_admin": isAdmin,
+	}
+
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserUpdate.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) SetUserExpiry(ctx context.Context, userID uuid.UUID, expiredAt time.Time) (*repository.User, error) {
+	userPayload := map[string]interface{}{
+		"expired_at": expiredAt,
+	}
+
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserUpdate.AppendError(err)
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) DeleteUser(ctx context.Context, userID uuid.UUID) (*repository.User, error) {
+	userPayload := map[string]interface{}{
+		"is_deleted": true,
+	}
+
+	var user repository.User
+	_, err := ur.Db.Model(&userPayload).TableExpr("users").Where("id = ?", userID).Returning("*").Update(&user)
+	if err != nil {
+		return nil, errors.FailedUserDelete.AppendError(err)
+	}
+
+	return &user, nil
 }
--- util/errors/pg.go
+++ util/errors/pg.go
@@ -8,6 +8,16 @@ import (
 )
 
 var (
-	// FailedUserCreate ...
-	FailedUserCreate = error.NewError(http.StatusInternalServerError, "PG3001", fmt.Errorf("Unable to create user"))
+	FailedUserExist    = error.NewError(http.StatusBadRequest, "PG3001", fmt.Errorf("User is already exist"))
+	FailedUserNotFound = error.NewError(http.StatusNotFound, "PG3002", fmt.Errorf("User cannot be found"))
+	FailedUserCreate   = error.NewError(http.StatusInternalServerError, "PG3003", fmt.Errorf("Unable to create user"))
+	FailedUserUpdate   = error.NewError(http.StatusInternalServerError, "PG3004", fmt.Errorf("Unable to update user"))
+	FailedUserDelete   = error.NewError(http.StatusInternalServerError, "PG3005", fmt.Errorf("Unable to delete user"))
+	FailedUserFetch    = error.NewError(http.StatusInternalServerError, "PG3006", fmt.Errorf("Unable to fetch user"))
+	FailedUsersFetch   = error.NewError(http.StatusInternalServerError, "PG3007", fmt.Errorf("Unable to fetch users"))
+)
+
+var (
+	FailedEmailExist    = error.NewError(http.StatusBadRequest, "PG3101", fmt.Errorf("Email is already exist"))
+	FailedUsernameExist = error.NewError(http.StatusBadRequest, "PG3102", fmt.Errorf("Username is already exist"))
 )
